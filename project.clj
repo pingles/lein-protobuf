5cf6d5e299be9634cbcaa5119f0033bde5d4c30e
Paul Ingles <paul@oobaloo.co.uk>
2014-01-22 13:44:20 +0000

    snapshot release to org.pingles for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-protobuf "0.4.1"
+(defproject org.pingles/lein-protobuf "0.4.2-SNAPSHOT"
   :description "Leiningen plugin for compiling protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
